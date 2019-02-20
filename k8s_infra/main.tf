32c48deba33e75e83ad5d516fd68c6464bd2efe2
Stefan Walker <stefan.walker74@gmail.com>
2019-02-20 12:48:27 +0100

    2 worker should be enough because mey notebook is limited

--- k8s_infra/main.tf
+++ k8s_infra/main.tf
@@ -1,31 +1,24 @@
 module "master1" {
   source = "./modules/worker_node"
-  cpu = 2
-  mem = 2048
+  cpu = 4
+  mem = 4096
   name = "master1"
 }
 
 module "worker1" {
   source = "./modules/worker_node"
-  cpu = 1
-  mem = 1024
+  cpu = 2
+  mem = 2048
   name = "worker1"
 }
 
 module "worker2" {
   source = "./modules/worker_node"
-  cpu = 1
-  mem = 1024
+  cpu = 2
+  mem = 2048
   name = "worker2"
 }
 
-module "worker3" {
-  source = "./modules/worker_node"
-  cpu = 1
-  mem = 1024
-  name = "worker3"
-}
-
 output "child_ip" {
-  value = "${module.master1.ip} ${module.worker1.ip} ${module.worker2.ip} ${module.worker3.ip}"
+  value = "\n            ${module.master1.ip}:\n            ${module.worker1.ip}:\n            ${module.worker2.ip}:"
 }
